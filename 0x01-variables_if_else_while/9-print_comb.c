daac902d60b90444254d02f87bcc352f0c0c416f
shewangiza <bereket.shewangizaw07@gmail.com>
2022-06-05 07:14:40 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <tdlib.h>
-#include <time.h>
 
 /**
  * main - prints all possible combinations of single-digit numbers
- *
  * Return: Always 0 (Success)
  */
 int main(void);
@@ -22,3 +19,4 @@ int main(void);
 	}
 	putchar('\n');
 	return (0);
+}
